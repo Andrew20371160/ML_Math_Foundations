1e9ffe313d4905ad2618e6e71db296e778c34285
Andrew Karam Saber <androw.20371160@eng.aun.edu.eg>
2024-04-27 04:26:00 +0300

    Update matrix_algebra.h

--- matrix_algebra/matrix_algebra.h
+++ matrix_algebra/matrix_algebra.h
@@ -1,5 +1,5 @@
-#ifndef VEC_H_INCLUDED
-#define VEC_H_INCLUDED
+#ifndef MATRIX_ALGEBRA_H_INCLUDED
+#define MATRIX_ALGEBRA_H_INCLUDED
 /*-update : added fix_pivots() this function rearranges the matrix rows
 so that the rows contatining the pivotsare on top and the rest of rows at bottom
 its crucial when using functions like lu_fact not really since i added permutation matrix earlier
@@ -11,10 +11,9 @@ but i wouldn't force that its on you :) .
 #include <iostream>
 #include <math.h>
 #include <string.h>
+#include "complex.h"
 using namespace std ;
-//feel free to edit this value the way you want
-const float tolerance = 0.000001 ;
-const float M_PI = 3.14159 ;
+
 //error messages
 string shape_error ="\nmatrices aren't the same shape default garbage value is -1\n";
 string square_error= "\nmatrix must be square to perform this operation default garbage value is -1\n";
@@ -24,21 +23,21 @@ string uninit_error = "\nmatrix isn't initialized yet\n";
 //write a function to test or use is_diag ,is_suare etc etc use them in one big function called
 //extract specifications of matrix for ex and this writes it into the matrix class so that
 //we won't have to do that again
+enum{upper_left=0,lower_left,lower_right,upper_right};
+
+template<typename DataType>
 class matrix{
 private :
 //2d array for holding data
-float  **vec ;
+DataType *vec ;
 //dimensions of matrix
 int rows , cols;
 //helper function for allocating memory for a 2d array
-float**get_vec(int r ,int c){
+DataType*get_vec(int r ,int c){
     //check for passed parameters
     if(r>0&&c>0){
         //memory allocation
-        float**ret_vec =new float*[r];
-        for(int i= 0  ; i <r ;i++){
-            ret_vec[i] = new float[c] ;
-        }
+        DataType*ret_vec =new DataType[r*c];
         return ret_vec ;
     }
     return NULL ;
@@ -47,9 +46,9 @@ public:
     //empty matrix
     matrix();//tested
     //by default initializes the matrix with zeroes
-    matrix(int rows ,int cols ,float initialization_value);//tested
+    matrix(int rows ,int cols ,DataType initialization_value=0);//tested
         //initializes a matrix with specified 1d array data
-    matrix(int rows ,int cols ,float* initialization_array,int size_of_array) ;
+    matrix(int rows ,int cols ,DataType* initialization_array,int size_of_array) ;
 
     //deallocate the memory allocated by the matrix
     ~matrix();//tested
@@ -60,9 +59,11 @@ public:
     //get cols value
     int get_cols()const;//tested
     //accessing element at a specified index
-    float& at(int r_ind,int c_ind);//tested
+    DataType& at(int r_ind,int c_ind);//tested
+        //accessing element at a specified index
+    DataType& at(int r_ind,int c_ind)const;//tested
     //fills a matrix with a certain value
-    bool fill(int r ,int c,float value);//tested
+    bool fill(int r ,int c,DataType value);//tested
     //sets current matrix as identity
     void identity(void);//tested
     //check for same dimensions for 2 matrices
@@ -70,57 +71,58 @@ public:
     //display functionality
     void show(void)const;//tested
     // Add a matrix to the caller matrix and returns it as a new matrix
-    matrix operator+(matrix&)const;//tested
+    matrix operator+(const matrix&)const;//tested
     // subtracts a matrix from the caller matrix and returns it as a new matrix
-    matrix operator-(matrix&)const;//tested
+    matrix operator-(const matrix&)const;//tested
     // Multiply caller matrix by another matrix
-    matrix operator*(matrix&)const;//tested
+    matrix operator*(const matrix&)const;//tested
     //returns caller/ sent matrix uses @inverse
-    matrix operator/(matrix&)const;//tested
+    matrix operator/(const matrix&)const;//tested
     // Multiply this matrix by a scalar
-    matrix operator*(float)const;//tested
+    matrix operator*(DataType)const;//tested
     //turns a matrix into a string for printing
     string mat_to_string(void) const;//tested
     //performs dot product of 2 matrices and returns the value
-    float dot(matrix&)const;//tested
+    DataType dot(const matrix&)const;//tested
     //performs alpha*x+y and returns the value
-    float axpy(float,matrix&)const;//tested
+    DataType axpy(DataType,const matrix&)const;//tested
     // Transpose caller matrix and returns it as a new matrix
     matrix transpose(void)const;//tested
+
     // Check if this matrix is square
     bool is_square(void)const;//tested
     // Calculate the determinant of this matrix
     // (only for square matrices)
-    float det(void);//tested
+    DataType det(void)const ;//tested
     // Calculate the inverse of this matrix and returns it as a new matrix
     // (only for square matrices)
-    matrix inverse(void);//tested
+    matrix inverse(void)const ;//tested
 
     // Calculate the trace of this matrix
     // (only for square matrices)
-    float trace(void)const;//tested
+    DataType trace(void)const;//tested
 
     // Calculate the rank of this matrix
-    int rank(void);//tested
+    int rank(void)const ;//tested
 
     // Check if this matrix is symmetric
-    bool is_symmetric(void);//tested
+    bool is_symmetric(void)const ;//tested
 
     // Check if this matrix is skew-symmetric
-    bool is_skew_symmetric(void);//tested
+    bool is_skew_symmetric(void)const ;//tested
 
     // Check if this matrix is orthogonal
-    bool is_orthogonal(void);//tested
+    bool is_orthogonal(void)const ;//tested
     //returns length of the caller
-    float norm2(void) ;//tested
+    DataType norm2(void) const ;//tested
     //same as previous but different wrapper
-    float length(void) ;//tested
+    DataType length(void) const ;//tested
     //theta between caller and passed matrix
-    float theta(matrix&) ;//tested
+    DataType theta(matrix&) const ;//tested
     //solves appended matrix using @gauss_down and then @back_sub
-    matrix solve(void);//tested
+    matrix solve(void)const ;//tested
     //performs axpy operation between 2 rows
-    void row_axpy(float alpha,int x_row,int y_row) ;
+    void row_axpy(DataType alpha,int x_row,int y_row) ;
 
     //performs gaussian elimination downward
     //added an optional input if you want to see where are the pivots
@@ -129,13 +131,13 @@ public:
     //if you want to store new pivots locations in the matrix
     //pass new_locations else old locations by default it returns new locations
     enum {old_locations =0,new_locations=1};
-    matrix gauss_down(matrix*pivots_indices,int pivots_locations);//tested
+    matrix gauss_down(matrix<int>*pivots_indices=NULL,int pivots_locations = new_locations)const ;//tested
     //performs gaussian elimination upward
-    matrix gauss_up(matrix *) ;//tested
+    matrix gauss_up(matrix<int>*)const  ;//tested
     //performs back substitution on a selected row and solution matrix is passed with it
-    float back_sub(int selected_row,matrix& solution_matrix);//tested
+    DataType back_sub(int selected_row,const matrix& solution_matrix)const ;//tested
     //performs forward substitution on a selected row and solution matrix is passed with it
-    float fwd_sub(int selected_row,matrix& solution_matrix) ;//tested
+    DataType fwd_sub(int selected_row,const matrix& solution_matrix)const ;//tested
     //yeah name is opvious
     bool switch_rows(int row1,int row2);//tested
     //performs lu factorization on a matrix
@@ -144,76 +146,76 @@ public:
     //elemination some rows could be switched so the permutation
     //matrix records that change its a simple identity with rows
     //switching as upper_fact rows switched
-    void lu_fact(matrix& lower_fact,matrix&permutation,matrix& upper_fact) ;//tested
+    void lu_fact(matrix& lower_fact,matrix&permutation,matrix& upper_fact)const ;//tested
     //assignment function
     //allows for matrix reuse
     void operator=(const matrix&) ;//tested
     //append 2 matrices by cols aka return matrix =[mat1 mat2]
-    matrix append_cols(const matrix&src) ;
+    matrix append_cols(const matrix&src)const ;
     //append 2 matrices by rows aka return matrix =[mat1]
     //                                             [mat2]
-    matrix append_rows(const matrix&src) ;
+    matrix append_rows(const matrix&src)const ;
     //checks if caller is perpindicular on the passed matrix
-    bool is_orthogonal(matrix&) ;//tested
+    bool is_orthogonal(const matrix&)const  ;//tested
     //checks if caller is in parallel with the passed matrix
-    bool is_parallel(matrix&) ;//tested
+    bool is_parallel(const matrix&)const  ;//tested
     //checks if 2 matrices are equal
-    bool operator==(matrix&) const;//tested
+    bool operator==(const matrix&) const;//tested
     // Check if this matrix is identity
-    bool is_identity(void);//tested
+    bool is_identity(void)const ;//tested
     // Check if this matrix is the zero matrix
-    bool is_zero(void);
+    bool is_zero(void)const ;
 
     // Check if this matrix is upper triangular
-    bool is_upper_tri(void);//tested
+    bool is_upper_tri(void)const ;//tested
 
     // Check if this matrix is lower triangular
-    bool is_lower_tri(void);//tested
+    bool is_lower_tri(void)const ;//tested
     // Check if this matrix is idempotent
-    bool is_idempotent(void);//tested
+    bool is_idempotent(void)const ;//tested
 
     // Check if this matrix is nilpotent
-    bool is_nilpotent(void);//tested
+    bool is_nilpotent(void)const ;//tested
 
     // Check if this matrix is involutory
-    bool is_involutory(void);//tested
+    bool is_involutory(void)const ;//tested
 
     // Check if this matrix is diagonal
-    bool is_diagonal(void);//tested
+    bool is_diagonal(void)const ;//tested
 
     // Check if this matrix is scalar
-    bool is_scalar(void);//tested
+    bool is_scalar(void)const ;//tested
 
     //this function checks if a current element is a pivot
     //it now returns pivot's row since its used in functions like lu_fact
-    int is_pivot(int row_index , int col_index);//tested
+    int is_pivot(int row_index , int col_index) ;//tested
     //same as is_pivot but used in gauss_up
     int is_pivot_up(int,int) ;
 
     //this function returns row reduced echolon form of a matrix
     //it mapps for each row the index of its pivot if found
     // if at that row there are no pivots its assigned the value -1
-    matrix rref(matrix & pivots_indices);//tested
+    matrix rref(matrix<int> * pivots_indices =NULL)const ;//tested
     // Checks if the set of vectors (columns of the matrix) is linearly independent.
-    bool is_independent(void);//tested
+    bool is_independent(void)const ;//tested
     // Calculates the dimension of the null space (kernel) of the matrix.
     // The null space consists of all vectors that are mapped to the zero vector by the linear transformation represented by the matrix.
-    int dim_null_cols(void);//tested
-    int dim_null_rows(void);//tested
+    int dim_null_cols(void)const ;//tested
+    int dim_null_rows(void)const ;//tested
     // Calculates the dimension of the column space (range) of the matrix.
     // The column space consists of all possible linear combinations of the column vectors in the matrix.
-    int dim(void);//tested
+    int dim(void)const ;//tested
     // Checks if the set of vectors (columns of the matrix) forms a basis for the vector space of the given dimension.
     // A set of vectors forms a basis if they are linearly independent and span the vector space.
-    bool is_basis(int dimension);//tested
+    bool is_basis(int dimension)const ;//tested
     // Returns a set of vectors (as a matrix) that forms a basis column space
-    matrix basis_cols(void);//tested
+    matrix basis_cols(void)const ;//tested
     // Returns a set of vectors (as a matrix) that forms a basis for the row space
-    matrix basis_rows(void);//tested
-    //returns Null space or row space
-    matrix null_rows(matrix*) ;
+    matrix basis_rows(void)const ;//tested
+    //returns Null space of row space
+    matrix null_rows(matrix*)const  ;
     //returns null space of column space
-    matrix null_cols(void) ;//tested
+    matrix null_cols(void)const  ;//tested
     //it fixes a matrix by putting rows that corresponds to pivots first
     //then rist of rows at th every end this fixes an issue at rref
     //where if a row switch occurs it won't be recorded in elementary matrix
@@ -228,29 +230,23 @@ public:
     so projection matrix where p =projection b
     projection = A*(AT*A)^-1 *AT
     */
-    matrix projection(void);
-    //fit a data set into a linear system
-    //Ax=b can't be solved
-    //AT*A* x* = AT*b
-    //now solvable
-    matrix fit_least_squares(matrix&data_set) ;
-    //extract a column of the caller and returns it as a column matrix
-    matrix extract_col(int index);
-    //this function takes a bunch of vectors in a matrix and returns
-    //the orthonormal vectors in a form of matrix (performs gram-shmidt algorithm)
-    matrix gram_shmidt(void);
-    //performs A -lambda * I where I is the identity
-    //then returns the matrix(more efficent than the normal A-lambda*I)
-    matrix SubLambdaI(float lambda);
-    //returns eigen vectors of a system using eigen values
-    //calculate null space for each A-lambda*I
-    //and append it to eigen vectors matrix
-    matrix eigen_vectors(matrix&eigen_values);
-    //returns cofactor of an element at position row_i,col_i
-    float cofactor(int row_i, int col_i) ;
-    //returns matrix of cofactors of all elements of the matrix
-    matrix cofactors(void) ;
+    matrix projection(void)const ;
 
+    matrix fit_least_squares(const matrix&data_set)const  ;
+
+    matrix extract_col(int index)const ;
+
+    matrix gram_shmidt(void)const ;
+    //returns cofactor of an element at position row_i,col_i
+    DataType cofactor(int row_i, int col_i)const  ;
+    //returns matrix of cofactors of all elements of the matrix
+    matrix cofactors(void)const  ;
+
+    matrix SubLambdaI(DataType lambda)const ;
+    matrix eigen_vectors(const matrix&eigen_values)const ;
+
+    matrix arrange(const matrix<int>&seq) ;
+    void at_quarter(int,const matrix&) ;
 };
 
 #endif // VEC_H_INCLUDED
