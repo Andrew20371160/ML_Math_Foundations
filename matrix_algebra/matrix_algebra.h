10e0398be2c3277fa5f38dce8ac2fe8b18ed2111
Andrew Karam Saber <androw.20371160@eng.aun.edu.eg>
2024-05-03 22:52:52 +0300

    Update matrix_algebra.h

--- matrix_algebra/matrix_algebra.h
+++ matrix_algebra/matrix_algebra.h
@@ -1,13 +1,12 @@
 #ifndef MATRIX_ALGEBRA_H_INCLUDED
 #define MATRIX_ALGEBRA_H_INCLUDED
-/*
-//tolerance is used in calculations like inverse and gram-shmidt ,gauss
-//this is sufficient for gram-shmidt since it requires a high degree of percision
-const double tolerance =1e-12;
-//check_tolerance is for is_identity , is_zero , aka every function starting with is_
-//modify this the way you want
-const double check_tolerance =1e-6;
-both consants are found in complex.h file
+/*-update : added fix_pivots() this function rearranges the matrix rows
+so that the rows contatining the pivotsare on top and the rest of rows at bottom
+its crucial when using functions like lu_fact not really since i added permutation matrix earlier
+but if you use elementary matrix from null_rows() and tried to test if elementary * matrix euqals
+rref(matrix) this sometimes isn't true since elementary matrix doesn't record the switches
+in rows that happens during the rref of the matrix its advisable to use after you initialize the matrix
+but i wouldn't force that its on you :) .
 */
 #include <iostream>
 #include <math.h>
@@ -35,11 +34,11 @@ class matrix{
 
 private :
 //feel free to edit this value the way you want
-//1d array for holding data
+//2d array for holding data
 DataType *vec ;
 //dimensions of matrix
 int rows , cols;
-//helper function for allocating memory for a 1d array
+//helper function for allocating memory for a 2d array
 DataType*get_vec(int r ,int c){
     //check for passed parameters
     if(r>0&&c>0){
@@ -61,6 +60,7 @@ public:
     ~matrix();//tested
     //copy constructor
     matrix(const matrix&);//tested
+
     //get rows value
     int get_rows()const;//tested
     //get cols value
@@ -220,8 +220,7 @@ public:
     matrix basis_cols(void)const ;//tested
     // Returns a set of vectors (as a matrix) that forms a basis for the row space
     matrix basis_rows(void)const ;//tested
-    //returns Null space of row space (optional if you want elementary matrix which turns the matrix into)
-    //its rref 
+    //returns Null space of row space
     matrix null_rows(matrix*elementary=NULL)const  ;
     //returns null space of column space
     matrix null_cols(void)const  ;//tested
@@ -296,6 +295,12 @@ public:
     //by default filters by check_tolerance
     void filter(double filter_tolerance=check_tolerance);
 
+    matrix operator^(unsigned int power);
+    //A = U S VT
+    void svd(matrix&u,matrix&s,matrix&vt);
+    //calculates the length of a column at a specified index
+    DataType col_length(int col_i);
+
 };
     //when calling do this
     //identity<DataType>
@@ -306,6 +311,7 @@ public:
     //rand<DataType>
     template <typename DataType>
     matrix<DataType> rand(int,int);
+
     //returns a column matrix representing
     //for i =0 to n-1 of w^i
     //used in fft
